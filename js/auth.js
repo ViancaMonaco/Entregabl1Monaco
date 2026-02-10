2d7f56aeac9e9de82c0a28e67fe0d40d1e62fddd
Vianca <vmonac@up.edu.ar>
2026-02-10 13:02:43 -0300

    Ajustes en la logica y agregado de error de user

---
+++ js/auth.js
@@ -0,0 +1,15 @@
+class User{
+    constructor(name, mail, username, password){
+        this.name = name
+        this.mail = mail
+        this.username = username
+        this.password = password
+    }
+}
+
+const usersArry = []
+
+
+export function logIn(user, pass){
+
+}
--- js/mainP.js
+++ js/mainP.js
@@ -1,24 +1,31 @@
 import { renderLogIn } from "./ui.js"
 
-function initLogIN(){
+function initLogIn(){
     const loginForm = document.getElementById("loginForm")
     const userInput = document.getElementById("user")
     const passInput = document.getElementById("pass")
 
     loginForm.onsubmit = (e) => {
-        e.preventDeFaul()
+        e.preventDefault()
+
         const usuario = userInput.value
         const password = passInput.value
-        LogIn(usuario, password)
+        const errorMsg = document.getElementById("loginError")
+        if (!logIn(usuario, password)){
+            errorMsg.style.display = "block"
+            return
+        }
+        errorMsg.style.display = "none"
     }
 }
+
 function main(){
     const loginBtn = document.getElementById("login")
     const registroBtn = document.getElementById("registro")
 
     loginBtn.onclick = () =>{
         renderLogIn() // UI: mostrar pantalla login
-        initLogIN() // Eventos: enganchar submit del formulario
+        initLogIn() // Eventos: enganchar submit del formulario
     }
 
     registroBtn.onclick = () => {
