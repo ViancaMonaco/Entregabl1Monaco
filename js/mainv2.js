5ea58ef7b4bfd040dd1d275fb58a6b3f85eaa6b5
Vianca <vmonac@up.edu.ar>
2026-02-10 13:16:08 -0300

    Cambios en el html y main v2

--- js/mainv2.js
+++ js/mainv2.js
@@ -26,6 +26,33 @@ function renderLogIn(){
     //Esto dice q muestre la nueva seccion de formulario logIn
 }
 
+function iniciarSesion(user, pass){
+
+    if (usuarios.length === 0) {
+        return false
+    }
+
+    const user = pedirUsuario()
+    const contra = pedirContraseña(user)
+
+    alert("Inicio de sesión exitoso")
+    return true
+}
+
+function initLogIN(){
+    const loginForm = document.getElementById("loginForm")
+    const userInput = document.getElementById("user")
+    const passInput = document.getElementById("pass")
+
+    loginForm.onsubmit = (e) => {
+        e.preventDeFaul()
+        const usuario = userInput.value
+        const password = passInput.value
+        iniciarSesion(usuario, password)
+    }
+}
+
+
 loginbtn.onclick = () =>{
     renderLogIn()
 }
