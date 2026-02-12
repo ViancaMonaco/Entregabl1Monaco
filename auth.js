cb0dc6a7c6cd2aeee61c999f4cdf17289dc7f1c5
Vianca <vmonac@up.edu.ar>
2026-02-12 12:48:53 -0300

    Eliminacion de archivos y carpetas borradores. Tambien se agrego la seccion del menu principal y el armado de la seccion de completar perfil

--- auth.js
+++ auth.js
@@ -4,11 +4,34 @@ function userExist(email){
     return users.some(u => u.email===email)
 }
 
+function getUser(email){
+    return users.find(u=>u.email === email)
+}
+
 function register(name, email, username, password){
-    if(userExist(email)){
-        return false
-    }
+    if(userExist(email)) return false
+    
     const newUser = new User(name,email,username,password)
     users.push(newUser)
+    return true
+}
+
+function login(email, pass){
+    if(!userExist(email)) return false
+    
+    const user = getUser(email)
+    return user.password === pass
+}
+
+function completeProfile(userEmail, height, weight, sex, birthYear, activityLevel, goal){
+    const user = getUser(userEmail)
+    if (!user) return false
+
+    user.profile.heightCm = height
+    user.profile.weights.push(weight)
+    user.profile.sex = sex
+    user.profile.brithYear = birthYear
+    user.profile.activityLevel = activityLevel
+    user.profile.goal = goal
     return true
 }
--- script.js
+++ script.js
@@ -12,14 +12,89 @@ function initRegister(){
         const username = usernameInput.value
         const password = passwordInput.value
         if(register(name, email, username, password)){
-            console.log("entre al if")
-            localStorage.setItem("loggedUser", username)
+            localStorage.setItem("loggedUser", email)
             hideSection("registerError")
         }
         else{
             showSection("registerError")
         }
     }
+}
+
+function initLogIn(){
+    const loginForm = document.getElementById("loginForm")
+    const emailInput = document.getElementById("emailLogin")
+    const passInput = document.getElementById("passLogin")
+
+    loginForm.onsubmit = (e) => {
+        e.preventDefault()
+        const email = emailInput.value
+        const pass = passInput.value
+        if(login(email,pass)){
+            localStorage.setItem("loggedUser", email)
+            hideSection("errorLogin")
+        }
+        else{
+            showSection("errorLogin")
+        }
+    }
+
+
+}
+
+function initProfileComplete(){
+    const profileForm = document.getElementById("profileForm")
+    const heightInput = document.getElementById("height")
+    const weightInput = document.getElementById("weight")
+    const sexInput = document.getElementById("sex")
+    const yearInput = document.getElementById("birthYear")
+    const activityLevelInput = document.getElementById("activityLevel")
+    const goalInput = document.getElementById("goal")
+
+    const backBtn = document.getElementById("btnBackMenu")
+
+    backBtn.onclick = () => {
+        hideSection("profileSection")
+        showSection("mainMenuSection")
+        return
+    }
+
+    profileForm.onsubmit = (e) =>{
+        e.preventDefault()
+        const userEmail = localStorage.getItem("loggedUser")
+        completeProfile(userEmail, heightInput.value, weightInput.value, sexInput.value, yearInput.value, activityLevelInput.value, goalInput.value)
+
+    }
+}
+
+function initMainMenu(){
+    const profileBtn = document.getElementById("btnCompleteProfile")
+    const recipesBtn = document.getElementById("btnRecipes")
+    const exerciesBtn = document.getElementById("btnExercises")
+    const progressBtn = document.getElementById("btnProgress")
+
+    profileBtn.onclick = () =>{
+        hideSection("mainMenuSection")
+        showSection("profileSection")
+        initProfileComplete()
+    }
+
+    recipesBtn.onclick = () => {
+        hideSection("mainMenuSection")
+    }
+
+    exerciesBtn.onclick = () => {
+        hideSection("mainMenuSection")
+    }
+
+    progressBtn.onclick = () => {
+        hideSection("mainMenuSection")
+    }
+}
+
+function mainMenu(){
+    showSection("mainMenuSection")
+    initMainMenu()
 }
 
 function income(){
@@ -30,12 +105,29 @@ function income(){
         hideSection("homeSection")
         showSection("registerSection")
         initRegister()
+        hideSection("registerSection")
+        mainMenu()
+
     }
 
     loginBtn.onclick = () =>{
         hideSection("homeSection")
         showSection("loginSection")
+        initLogIn()
+        hideSection("loginSection")
+        mainMenu()
+
     }
 }
 
-income()
+function main(){
+    const loggedUser = localStorage.getItem("loggedUser")
+    if (loggedUser) {
+        hideSection("homeSection")
+        mainMenu()
+    } else {
+        income()
+    }
+}
+
+main()
